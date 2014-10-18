9d115d9d9f104de448f8dbde69b80eb857376aae
felixr <felixramosmusic@yahoo.es>
2014-10-18 11:13:06 +0200

    solucion

--- Practica04/Asigna.s
+++ Practica04/Asigna.s
@@ -4,3 +4,27 @@
 	.type Asignar,@function
 Asignar:
         # Aqui viene vuestro codigo
+	pushl	%ebp
+	movl	%esp, %ebp
+	
+	pushl	%ebx
+	pushl	%esi
+	
+	movl	8(%ebp), %ebx
+	imul	$12, 24(%ebp), %esi
+	
+	movb	12(%ebp), %al
+	movb	%al, (%ebx, %esi)
+	
+	movl	16(%ebp), %eax
+	movl	%eax, 4(%ebx, %esi)
+	
+	movl	20(%ebp), %eax
+	movl	%eax, 8(%ebx, %esi)
+	
+	popl	%esi
+	popl	%ebx
+	
+	movl	%ebp, %esp
+	popl	%ebp
+	
--- Practica04/Inserta.s
+++ Practica04/Inserta.s
@@ -4,3 +4,70 @@
 	.type Insertar,@function
 Insertar:
         # Aqui viene vuestro codigo
+	pushl	%ebp
+	movl	%esp, %ebp
+  subl	$12, %esp
+	
+	pushl	%ebx
+	pushl	%esi
+	pushl	%edi
+	
+	movl	$0, %esi          # i=0 --> %esi
+	movl	$-1, %ecx         # lug=-1 --> %ecx
+	movl	8(%ebp), %ebx     # @v --> %ebx
+do:
+	imul	$12, %esi, %edx		# i*12 --> %edx
+	movl	4(%ebx,%edx), %eax
+if1:
+	cmpl	%eax, 16(%ebp)
+	jge		else
+	movl	%esi, %ecx
+	jmp		if2
+else:
+	incl	%esi
+	cmpl	$80000000, %eax
+	jne		do
+if2:
+	cmpl	$80000000, %eax
+	jne		while
+	movl	%esi, %ecx
+while:
+	cmpl	$80000000, %eax
+	je		endwhile
+	incl	%esi
+endwhile:
+	movl	%esi, %edi        # j=i --> %edi
+for:
+	cmpl	%ecx, %edi
+	jl		endfor
+	imul	$12, %edi, %edx		# j*12 --> %edx
+	movb	(%ebx, %edx), %al
+	movb	%al, 12(%ebx, %edx)
+	movl	4(%ebx, %edx), %eax
+	movl	%eax, 16(%ebx, %edx)
+	movl	8(%ebx, %edx), %eax
+	movl	%eax, 20(%ebx, %edx)
+	decl	%edi              # j--
+	jmp for
+endfor:
+	# paso de parametros
+	pushl	%ecx              # lug --> M[%esp]
+	pushl	20(%ebp)          # X.m --> M[%esp]
+	pushl	16(%ebp)          # X.k --> M[%esp]
+	pushl	12(%ebp)          # X.c --> M[%esp]
+	pushl	%ebx              # @v --> M[%esp]
+	
+	call	Asignar
+	addl	$20, %esp
+	
+	incl	%esi
+	movl	%esi, %eax
+
+	popl	%edi
+	popl	%esi
+	popl	%ebx
+  
+	movl	%ebp, %esp
+	popl	%ebp
+	ret
+	
